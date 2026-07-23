983c20c8373dd744ccdd50d40995021215e6cbc0
akunnpc <rivkysingkong@gmail.com>
2026-07-23 22:11:35 +0700

    Fix: Update test to expect correct app_name "NimbusV3"

--- app/src/test/java/com/example/ExampleRobolectricTest.kt
+++ app/src/test/java/com/example/ExampleRobolectricTest.kt
@@ -20,7 +20,7 @@ class ExampleRobolectricTest {
     fun `read string from context`() {
         val context = ApplicationProvider.getApplicationContext<Context>()
         val appName = context.getString(R.string.app_name)
-        assertEquals("Shield Browser", appName)
+        assertEquals("NimbusV3", appName)
     }
 
     @Test
